5ca274cec54e36f73f08df0cbb7dd0ac59768b58
AbdullahRashid99 <abdullah.business4444@gmail.com>
2025-08-10 22:52:30 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Portfolio from './Portfolio.jsx';
 import Services from './Services.jsx';
 
