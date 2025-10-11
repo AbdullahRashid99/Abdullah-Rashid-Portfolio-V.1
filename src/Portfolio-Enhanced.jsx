52a6b58b4f2244f7dcfd79ee8efbb3d00b79dfe0
AbdullahRashid99 <abdullah.business4444@gmail.com>
2025-10-11 16:49:23 +0300

    Update Portfolio-Enhanced.jsx

--- src/Portfolio-Enhanced.jsx
+++ src/Portfolio-Enhanced.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import {
-  Mail, trending-up, Briefcase, Star, Folder, Menu, X, Send, Linkedin, Phone,
-  Award, Target, Megaphone, hand-helping, UserCheck, Building, LineChart,
+  Mail, TrendingUp, Briefcase, Star, Folder, Menu, X, Send, Linkedin, Phone,
+  Award, Target, Megaphone, HandHelping, UserCheck, Building, LineChart,
   Camera, GraduationCap, ArrowRight, Palette, Code, BarChart3,
   Tiktok, Instagram, Dribbble, Twitter, ArrowUp,
   Store as IconShopify,
@@ -230,8 +230,8 @@ const ModalBackdrop = ({ children, onClose }) => (
 
 const servicesList = [
   { title: 'Creat Shopify Store', icon: <Store size={48} />, link: 'https://forms.gle/SfkP6rgmFm2oVPs79' },
-  { title: 'Get Consultations', icon: <hand-helping size={48} />, link: 'https://forms.gle/C1pAyQRi2fmxzFEK8' },
-  { title: 'Scale Brand', icon: <trending-up size={48} />, link: 'https://forms.gle/CzZKmtHBNmXWeKDHA' },
+  { title: 'Get Consultations', icon: <HandHelping size={48} />, link: 'https://forms.gle/C1pAyQRi2fmxzFEK8' },
+  { title: 'Scale Brand', icon: <TrendingUp size={48} />, link: 'https://forms.gle/CzZKmtHBNmXWeKDHA' },
 ];
 
 function ServicesModal({ onClose }) {
