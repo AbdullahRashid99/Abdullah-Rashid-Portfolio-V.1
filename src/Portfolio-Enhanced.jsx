3a1ac3d3aadff6be6869a22a74c9a3ddb530fb09
AbdullahRashid99 <abdullah.business4444@gmail.com>
2025-10-11 15:45:18 +0300

    Update Portfolio-Enhanced.jsx

--- src/Portfolio-Enhanced.jsx
+++ src/Portfolio-Enhanced.jsx
@@ -231,7 +231,7 @@ const ModalBackdrop = ({ children, onClose }) => (
 const servicesList = [
   { title: 'Creat Shopify Store', icon: <IconShopify size={48} />, link: 'https://forms.gle/SfkP6rgmFm2oVPs79' },
   { title: 'Get Consultations', icon: <HelpCircle size={48} />, link: 'https://forms.gle/C1pAyQRi2fmxzFEK8' },
-  { title: 'Scale Brand' Scale or Startup', icon: <Users size={48} />, link: 'https://forms.gle/CzZKmtHBNmXWeKDHA' }
+  { title: 'Scale Brand', icon: <Users size={48} />, link: 'https://forms.gle/CzZKmtHBNmXWeKDHA' },
 ];
 
 function ServicesModal({ onClose }) {
