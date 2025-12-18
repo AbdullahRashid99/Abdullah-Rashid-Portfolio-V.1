4190a3001132cf5a4e72326ad053308295f747e0
AbdullahRashid99 <abdullah.business4444@gmail.com>
2025-12-19 01:58:36 +0200

    Update Portfolio-Enhanced.jsx

--- src/Portfolio-Enhanced.jsx
+++ src/Portfolio-Enhanced.jsx
@@ -287,8 +287,7 @@ const MultiStripBanners = () => {
 
   const row1 = ["https://i.postimg.cc/C5GsYm88/11.png", "https://i.postimg.cc/wMXQH0N1/8.png", "https://i.postimg.cc/qqsx0jK6/10.png"];
   const row2 = ["https://i.postimg.cc/L5t3RNPm/1.png", "https://i.postimg.cc/cCRBZX34/2.png", "https://i.postimg.cc/90dYVJ9W/3.png", "https://i.postimg.cc/7h3nDmzH/4.png"];
-  const row3 = ["https://i.postimg.cc/D0rPFBGm/5.png", "https://i.postimg.cc/mkfy00Pg/Untitled-design-(1).png", "https://i.postimg.cc/Zn8xZVNp/12.png", "https://i.postimg.cc/Xqfk3Q5G/9.png
-"];
+  const row3 = ["https://i.postimg.cc/D0rPFBGm/5.png", "https://i.postimg.cc/mkfy00Pg/Untitled-design-(1).png", "https://i.postimg.cc/Zn8xZVNp/12.png", "https://i.postimg.cc/Xqfk3Q5G/9.png"];
 
   return (
     <div className="space-y-4 md:space-y-8">
