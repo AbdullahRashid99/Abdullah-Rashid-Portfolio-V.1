0ed1947cffdfb2de5be16a628e4cf5fa4af89830
AbdullahRashid99 <abdullah.business4444@gmail.com>
2025-12-18 02:56:10 +0200

    Update Portfolio-Enhanced.jsx

--- src/Portfolio-Enhanced.jsx
+++ src/Portfolio-Enhanced.jsx
@@ -57,7 +57,7 @@ const skillsData = [
 
 // --- Projects Data ---
 const projectsData = [
-  { title: "Fashion", image: "https://drive.google.com/file/d/15ZQOSFftHS02Zb4Hymh5Td7NxOZGQyrO/view?usp=drive_link" },
+  { title: "Fashion", image: "https://drive.google.com/file/d/1tfyW9W7IN9TBEPpB3ysyhNHASJBeZtmD/view?usp=sharing" },
   { title: "Cosmetics", image: "https://www.dhl.com/discover/content/dam/hong-kong/desktop/e-commerce-advice/e-commerce-guides-by-country/guide-to-packaging-and-shipping-cosmetics-and-beauty-products-from-hong-kong/cosmetic-and-beauty-products-in-a-shipping-box-1920x998.jpg" },
 ];
 
